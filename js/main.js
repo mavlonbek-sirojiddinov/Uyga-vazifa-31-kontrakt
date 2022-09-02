b6dca63c15d8d21bf8747c5578aab6903eac53e6
mavlonbek-sirojiddinov <mavlonbeksirojiddinov33@gmail.com>
2022-09-02 13:42:52 +0500

    add birbalolar

--- js/main.js
+++ js/main.js
@@ -1,29 +1,38 @@
 
 var elSiteForm = document.querySelector('.site-form');
-var elNameInput = document.querySelector('#name-input')
-var elBallInput = document.querySelector('#ball-input')
-var elBtnjs = document.querySelector('.btn-js')
+var elNameInput = document.querySelector('#name-input');
+var elBallInput = document.querySelector('#ball-input');
+var elBtn = document.querySelector('.btn-js');
+var elMsBox = document.querySelector('.massege-box')
 var elMassege = document.querySelector('.massege');
-var ball = 106;
+var elMassege2 = document.querySelector('.massege-2')
+var elMassege3 = document.querySelector('.massege-3')
 
-elSiteForm.addEventListener('submit', function (evt){
-  evt.preventDefault()
+elSiteForm.addEventListener('submit', function (e){
+  e.preventDefault()
+  
+  var elNameInput = document.querySelector('#name-input').value.trim().toUpperCase();
+  var elBallInput = parseFloat(elSiteForm.querySelector('#ball-input').value.trim(), 10);
+  var elBallNum = 106;
+  var elBallNan = 57;
 
-  var elNameInput = elSiteForm.querySelector('#name-input').Value.trim();
-  var elBallInput =parseFloat(elSiteForm.querySelector('#ball-input').value.trim(), 10);
-
-  if (elBallInput >= ball) {
-    elMassege.textContent = `Oq yo'l, ${elNameInput}`
+  if (elBallInput >= elBallNum) {
+    elMsBox.style.backgroundColor = 'rgb(21, 255, 0)'
+    elMassege.textContent = `Tabriklaymiz ${elNameInput}`
+    elMassege2.textContent = `siz KONTRAKT asosida o'qishga kirdingiz`
+    elMassege3.textContent = `Your score ${elBallInput} nice !`
+  } else if (elBallInput >= elBallNan) {
+    elMsBox.style.backgroundColor = '#ffe600'
+    elMassege.textContent = `Tabriklaymiz ${elNameInput}`
+    elMassege2.textContent = `siz super KONTRAKT asosida o'qishga kirdingiz`
+    elMassege3.textContent = `Your score ( ${elBallInput} ) good !`
   } else {
-
+    elMsBox.style.backgroundColor = 'rgb(255, 36, 36)'
+    elMassege.textContent = `Tabriklamaymiz ${elNameInput}`
+    elMassege2.textContent = `siz o'qishga kira olmadingiz`
+    elMassege3.textContent = `Your score ( ${elBallInput} ) bad !`
   }
-});
-
-
-
-
-
-
+})
 
 
 
